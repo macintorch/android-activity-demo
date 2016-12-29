3fbb023610f22615bc756561854850ec7572d05b
Ainor Syahrizal <macintorch@gmail.com>
2016-12-29 15:16:35 +0800

    initial commit

---
+++ app/src/main/java/ainor/com/my/multipleactivitiesdemo/SecondActivity.java
@@ -0,0 +1,21 @@
+package ainor.com.my.multipleactivitiesdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public void toFirstActivity (View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
